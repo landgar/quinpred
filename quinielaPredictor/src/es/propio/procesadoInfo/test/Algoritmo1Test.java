c0b4c1195a7fc65e9a9177d64297ed2cbf540b59
landgar <luisandresgarcia@gmail.com>
2012-12-06 10:53:09 +0100

    librería matemática

--- quinielaPredictor/src/es/propio/procesadoInfo/test/Algoritmo1Test.java
+++ quinielaPredictor/src/es/propio/procesadoInfo/test/Algoritmo1Test.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
 import es.propio.cargadorInfoWeb.CargadorWebNombresProximaQuiniela;
 import es.propio.modeladoInfo.Boleto;
 import es.propio.modeladoInfo.Division;
@@ -28,15 +30,16 @@ public class Algoritmo1Test {
 	public static void main(String[] args) throws Exception {
 		System.out.println("TEST Algoritmo1Test");
 		Algoritmo1 alg = new Algoritmo1();
-		calcula(alg);
-		alg.pintame();
+		 calcula(alg);
+		 alg.pintame();
+		generadorAleatorio();
 	}
 
 	private static void calcula(Algoritmo1 alg) throws Exception {
-		//Carga de boleto
+		// Carga de boleto
 		CargadorWebNombresProximaQuiniela cargadorBoleto = new CargadorWebNombresProximaQuiniela();
 		cargadorBoleto.cargar("24");
-		//Traspaso de partidos a listas para su predicción 
+		// Traspaso de partidos a listas para su predicción
 		List<PronosticoPartido> listaPartidosPrimera = new ArrayList<PronosticoPartido>();
 		List<PronosticoPartido> listaPartidosSegunda = new ArrayList<PronosticoPartido>();
 		PronosticoPartido pronostico;
@@ -55,7 +58,7 @@ public class Algoritmo1Test {
 				listaPartidosSegunda.add(pronostico);
 			}
 		}
-		//Predicción de resultados
+		// Predicción de resultados
 		PronosticoJornada pronosticoPrimera = new PronosticoJornada(
 				listaPartidosPrimera, 1, IdAlgoritmoEnum.ALGORITMO1);
 		alg.setEstimacionJornadaPrimera(pronosticoPrimera);
@@ -65,4 +68,21 @@ public class Algoritmo1Test {
 		alg.setEstimacionJornadaSegunda(pronosticoSegunda);
 		alg.calcularPronosticoSegunda();
 	}
+
+	public static void generadorAleatorio() {
+		System.out.println("Generador aleatorio de valores normalizados: ");
+		// Get a DescriptiveStatistics instance
+		DescriptiveStatistics stats = new DescriptiveStatistics();
+		// Add the data from the array
+		for (int i = 0; i < 1000; i++) {
+			stats.addValue(Algoritmo1.generateNormalizedRandomNumber());
+		}
+		Double mean = stats.getMean();
+		Double std = stats.getStandardDeviation();
+		Double median = stats.getPercentile(50);
+		System.out.println("Media: " + mean);
+		System.out.println("Desviación típica: " + std);
+		System.out.println("Mediana: " + median);
+	}
+
 }
