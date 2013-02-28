037053c34bbaeaf7c661f060d1ab2b32f5ec941c
Luis Andrés García <luisandresgarcia@gmail.com>
2013-02-28 23:54:03 +0100

    detalle de empates

--- quinielaPredictor/src/es/propio/procesadoInfo/Algoritmo5.java
+++ quinielaPredictor/src/es/propio/procesadoInfo/Algoritmo5.java
@@ -66,7 +66,7 @@ public class Algoritmo5 extends AbstractAlgoritmo {
 		if (ACTITUD_AGRESIVA)
 			anadirEmpates(getEstimacionJornadaPrimera().getNumeroJornada(),
 					getTemporadaPrimera(),
-					ParametroNombre.DIFERENCIA_POSICIONES_EN_CLASIFICACION, 0,
+					ParametroNombre.DIFERENCIA_POSICIONES_EN_CLASIFICACION, 1,
 					lista);
 	}
 
